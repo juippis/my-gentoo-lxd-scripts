ea111e55d7548abb94382aa0668114d53495526f
Joonas Niilola <juippis@gentoo.org>
2021-04-27 17:56:09 +0300

    container/bin/errors_and_qa_notices.sh: add file
    prtester.sh: Find all build errors fast, find all QA notices after emerge for the
    atom we just tested.
    
    rdeptester.sh: With pkg-testing-tool --report, and with a custom "test.conf",
    find out exact errors easier.

---
+++ container/bin/errors_and_qa_notices.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# prtester.sh: Find all build errors fast, find all QA notices after emerge for the 
+# atom we just tested.
+#
+# rdeptester.sh: With pkg-testing-tool --report, and with a custom "test.conf", 
+# find out exact errors easier.
+#
+
+echo "Grepping through the logs for any errors or QA notices..."
+grep -r "QA Notice:" /var/tmp/portage/vbslogs/build/
+grep -r "ERROR:" /var/tmp/portage/vbslogs/build/ | grep "failed"
+grep -r "* Failed to" /var/tmp/portage/vbslogs/build/
+grep -r "is deprecated, use .* instead" /var/tmp/portage/vbslogs/build/
+grep -r "installing to one or more unexpected paths" /var/tmp/portage/vbslogs/build/
+grep -r "Permission denied" /var/tmp/portage/vbslogs/build/
+grep -r "command not found" /var/tmp/portage/vbslogs/build/
+grep -r "WARNING: Unknown options:" /var/tmp/portage/vbslogs/build/
+echo "Finished grepping."
+
