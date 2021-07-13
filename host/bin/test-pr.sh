64353768b6117a898ff5df68fd114db6d226dc11
Joonas Niilola <juippis@gentoo.org>
2021-07-13 17:19:49 +0300

    host/bin/test-pr.sh: update script path

--- host/bin/test-pr.sh
+++ host/bin/test-pr.sh
@@ -10,9 +10,9 @@ echo "Initializing new test environment... this will take a while."
 lxc copy my-test-container my-test-container-snap-"${1}"
 lxc start my-test-container-snap-"${1}"
 lxc exec my-test-container-snap-"${1}" -- su --login -lc "(sleep 10 && cd /var/db/repos/gentoo && curl -s -L https://patch-diff.githubusercontent.com/raw/gentoo/gentoo/pull/${1}.patch | git -c user.email=my@email -c user.name=MyName am --keep-cr -3)"
-lxc exec my-test-container-snap-"${1}" -- su --login -lc "(sleep 10 && ~/bin/prtester.sh)"
+lxc exec my-test-container-snap-"${1}" -- su --login -lc "(sleep 10 && ~/lxd-bin/container/bin/prtester.sh)"
 lxc exec my-test-container-snap-"${1}" -- su --login -lc "(sleep 10 && pfl &>/dev/null)"
-lxc exec my-test-container-snap-"${1}" -- su --login -lc "(~/bin/errors_and_qa_notices.sh)"
+lxc exec my-test-container-snap-"${1}" -- su --login -lc "(~/lxd-bin/container/bin/errors_and_qa_notices.sh)"
 lxc exec my-test-container-snap-"${1}" -- bash
 lxc stop my-test-container-snap-"${1}"
 
