eaf53b48a0b2833d70e3f86d8d8b6dd0df06201b
Joonas Niilola <juippis@gentoo.org>
2021-08-15 11:51:15 +0300

    container/bin/container_maintain_my-test-container.sh: ignore bdeps
     - this change makes daily base-container maintenance a bit lengthier,
       but with binpackages we should have fast access to build-time deps
       already. In return, it'll expose more ebuilds with missing
       build-time deps.

--- host/bin/container_maintain_my-test-container.sh
+++ host/bin/container_maintain_my-test-container.sh
@@ -5,9 +5,9 @@
 lxc start my-test-container
 lxc exec my-test-container -- su --login -lc "(sleep 10 && cd ~/lxd-bin && git pull)"
 lxc exec my-test-container -- su --login -lc "(cd ~/ionen-dev-scripts && git pull)"
-lxc exec my-test-container -- su --login -lc "(emerge --sync)"
-lxc exec my-test-container -- su --login -lc "(emerge -uvDN --with-bdeps=y --binpkg-changed-deps=y --keep-going world && emerge --depclean)"
-lxc exec my-test-container -- su --login -lc "(eclean-kernel -n 1)"
+lxc exec my-test-container -- su --login -lc "emerge --sync"
+lxc exec my-test-container -- su --login -lc "(emerge -uvDN --binpkg-changed-deps=y --keep-going world && emerge --depclean --with-bdeps=n)"
+lxc exec my-test-container -- su --login -lc "eclean-kernel -n 1"
 lxc exec my-test-container -- su --login -lc "(eselect news read && etc-update)"
 lxc exec my-test-container -- su --login -lc "pfl"
 lxc exec my-test-container -- su --login -lc "eclean packages --changed-deps"
