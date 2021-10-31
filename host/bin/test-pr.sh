2774ca7c34ade064948f672db5d515789c2d0336
Joonas Niilola <juippis@gentoo.org>
2021-10-31 09:10:40 +0200

    host/bin/test-pr.sh: add very simple "error handling"
     - and convert ${1} to ${id} for future enhancements (see TODO).

--- host/bin/test-pr.sh
+++ host/bin/test-pr.sh
@@ -4,15 +4,21 @@
 # Input: test-pr.sh 12345
 # Would test https://github.com/gentoo/gentoo/pull/12345
 
+if [[ -z ${1} ]]; then
+	echo "Please insert a GitHub PR number!"
+	echo "https://github.com/gentoo/gentoo/pull/<this_one>"
+	exit
+fi
+
 id="${1}"
 
 echo "Initializing new test environment... this will take a while."
-lxc copy my-test-container my-test-container-snap-"${1}"
-lxc start my-test-container-snap-"${1}"
-lxc exec my-test-container-snap-"${1}" -- su --login -lc "(sleep 10 && cd /var/db/repos/gentoo && curl -s -L https://patch-diff.githubusercontent.com/raw/gentoo/gentoo/pull/${1}.patch | git -c user.email=my@email -c user.name=MyName am --keep-cr -3)"
-lxc exec my-test-container-snap-"${1}" -- su --login -lc "(sleep 10 && ~/lxd-bin/container/bin/prtester.sh)"
-lxc exec my-test-container-snap-"${1}" -- su --login -lc "(sleep 10 && pfl &>/dev/null)"
-lxc exec my-test-container-snap-"${1}" -- su --login -lc "(~/lxd-bin/container/bin/errors_and_qa_notices.sh)"
-lxc exec my-test-container-snap-"${1}" -- su --login
-lxc stop my-test-container-snap-"${1}"
+lxc copy my-test-container my-test-container-snap-"${id}"
+lxc start my-test-container-snap-"${id}"
+lxc exec my-test-container-snap-"${id}" -- su --login -lc "(sleep 10 && cd /var/db/repos/gentoo && curl -s -L https://patch-diff.githubusercontent.com/raw/gentoo/gentoo/pull/${id}.patch | git -c user.email=my@email -c user.name=MyName am --keep-cr -3)"
+lxc exec my-test-container-snap-"${id}" -- su --login -lc "(sleep 10 && ~/lxd-bin/container/bin/prtester.sh)"
+lxc exec my-test-container-snap-"${id}" -- su --login -lc "(sleep 10 && pfl &>/dev/null)"
+lxc exec my-test-container-snap-"${id}" -- su --login -lc "(~/lxd-bin/container/bin/errors_and_qa_notices.sh)"
+lxc exec my-test-container-snap-"${id}" -- su --login
+lxc stop my-test-container-snap-"${id}"
 
