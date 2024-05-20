542641feaf314d5f19f35c93d89ca56c126efda9
Lucio Sauer <watermanpaint@posteo.net>
2024-05-20 22:40:43 +0200

    container-bin/prtester.sh: simplify finding ebuilds for testing
    Git can do most of the heavy lifting for figuring out, which files
    changed in what way.
    We only want new (A), modified (M) and renamed (R) files and pick out
    the ebuilds.
    
    Improve testing loop readability
      No need to append the iteration count to the report, since all members
      in pkgstobetested are unique.
      Simplify atom extraction from catpkg using Bash builtins.
    
    Signed-off-by: Lucio Sauer <watermanpaint@posteo.net>

--- container/bin/prtester.sh
+++ container/bin/prtester.sh
@@ -11,40 +11,30 @@
 
 cd /var/db/repos/gentoo || exit
 
-# Create an array of commits
-mapfile -t newcommits < <(git rev-list origin/HEAD..HEAD)
-declare -a pkgstobetestedtmp1
-declare -a pkgstobetestedfinalarray
-
-if [[ ${#newcommits[@]} -eq 0 ]]; then
+# git rev-list --quiet doesn't seem to work as intended (v2.44.1)
+if [[ -z "$(git rev-list origin/HEAD..HEAD)" ]]; then
 	echo "Couldn't find new commits to test. Exiting..."
 	exit
 fi
 
-# Please send help with the grepping...
-# /^[+-]{3} [a-z]\/([^\/]+)\/[^\/]+\/(.*)\.ebuild/
-# | grep -E '\+\+\+' | grep ".ebuild" | cut -d  "/" -f 2,4 | awk -F  '.ebuild' '{print $1}'
-# | grep -E '^[+]{3} [a-z]/([^\/]+)/[^\/]+/(.*)\.ebuild$' | sed 's#^[+-]\{3\} [a-z]/ \([^/]\+\)/[^/]\+/\(.*\)\.ebuild#\1/\2#'
-for i in "${newcommits[@]}"; do
-	pkgstobetestedtmp1+=( $(git show "${i}" | grep -E '^[+]{3} [a-z]/([^\/]+)/[^\/]+/(.*)\.ebuild$' | sed 's#^[+]\{3\} [a-z]/\([^/]\+\)/[^/]\+/\(.*\)\.ebuild#\1/\2#') )
-	pkgstobetestedtmp1+=( $(git show "${i}" | grep "rename to" | grep ".ebuild" | cut -d " " -f3 | cut -d  "/" -f 1,3 | awk -F  '.ebuild' '{print $1}') )
-done
-
-# Remove any duplicate entries, we only need to test the final ebuild once.
-declare -A tmpsortarray
-for i in "${pkgstobetestedtmp1[@]}"; do tmpsortarray["${i}"]=1; done
-mapfile -t pkgstobetestedfinalarray < <(printf '%s\n' "${!tmpsortarray[@]}")
+pkgstobetested=(
+	$(git show --name-only --diff-filter=AMR --format=tformat: \
+	origin/HEAD..HEAD | sort -u | grep ebuild)
+)
 
 # Let's print what we're about to test.
-echo "Packages to be tested:"
-echo "${pkgstobetestedfinalarray[@]}"
+echo "Ebuilds to be tested:"
+echo "${pkgstobetested[@]}"
 echo ""
 
-for (( j=0; j<${#pkgstobetestedfinalarray[@]}; j++ )); do
-	atom=$(echo "${pkgstobetestedfinalarray[${j}]}" | cut -d  "/" -f 2)
-	pkg-testing-tool --append-emerge '--autounmask=y --oneshot' --extra-env-file 'test.conf' \
-		--test-feature-scope once --max-use-combinations 6 --report /var/tmp/portage/vbslogs/"${atom}"-"${j}".json \
-		-p "=${pkgstobetestedfinalarray[${j}]}"
+for pkg in "${pkgstobetested[@]}"; do
+	atom="${pkg##*/}"
+	atom="${atom%.ebuild}"
+	pkg-testing-tool --append-emerge '--autounmask=y --oneshot' \
+		--extra-env-file 'test.conf' \
+		--test-feature-scope once --max-use-combinations 6 \
+		--report /var/tmp/portage/vbslogs/"${atom}".json \
+		-f "${pkg}"
 done
 
 echo "Error reports for failed atoms, use errors_and_qa_notices.sh to find out exact errors:"
